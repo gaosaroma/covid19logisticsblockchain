06d00c399a70aea2e265613de8bc8dddd0642324
gaosaroma <gaosaroma@gmail.com>
2020-12-08 21:41:25 +0800

    init

---
+++ src/main/java/tjsse20/covid19logisticsblockchain/Covid19logisticsblockchainApplication.java
@@ -0,0 +1,13 @@
+package tjsse20.covid19logisticsblockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19logisticsblockchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Covid19logisticsblockchainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tjsse20/covid19logisticsblockchain/Covid19logisticsblockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package tjsse20.covid19logisticsblockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19logisticsblockchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
