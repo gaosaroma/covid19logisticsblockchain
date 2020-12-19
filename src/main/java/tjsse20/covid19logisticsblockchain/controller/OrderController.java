1975ec927ad45d178af99c39bab03a5d0c2a72d3
Tanrui <1063932362@qq.com>
2020-12-19 20:54:01 +0800

    add controllers

---
+++ src/main/java/tjsse20/covid19logisticsblockchain/controller/OrderController.java
@@ -0,0 +1,9 @@
+package tjsse20.covid19logisticsblockchain.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+}
---
+++ src/main/java/tjsse20/covid19logisticsblockchain/controller/RiskController.java
@@ -0,0 +1,10 @@
+package tjsse20.covid19logisticsblockchain.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/risk")
+public class RiskController {
+}
